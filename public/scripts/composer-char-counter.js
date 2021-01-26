0385600e037cb96d1fc7740db7c960098ef87c4c
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-26 23:36:43 +0000

    added js page to manage character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  // $(this).on("input", function (event) {
+  //   console.log(event);//input event and all data about event
+  //   console.log(event.target)//selects textarea specifically
+  //   console.log(event.target.parent.counter); //
+  //   console.log(event.originalEvent.data); //key pressed
+  //   if (event.originalEvent.data !== null) {
+  //     $('#counter').on('input', function () {
+  //       counter.val()--;
+  //     })
+  //   } else if (event.originalEvent.data === null) {
+  //     counter++;
+  //   }
+  const count = $(".counter").val();
+  $('#tweet-text').keyup(() => {
+    let string = $("#tweet-text").val();
+    $('.counter').val(count - string.length)
+  });
+  //add in colour changing
+});
+
+
+//should try to replace direct references to counter using this
+//will likely need to use binding to keep this right
+
+
+// document.addEventListener('input', function () {
+//   console.log(this);
+// });
