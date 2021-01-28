33dcc49d1871a1e0c520a51f87dbf36b504ec6a4
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-28 22:52:36 +0000

    added responsiveness for viewing on tablet/desktop, can still work on this

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -17,12 +17,12 @@ $(document).ready(function() {
     $('.counter').val(count - string.length)
   });
   //add in colour changing
-  if (count < 0) {
-    $(`.counter`).css('color', 'red')
-  }
-  if (count >= 0) {
-    $(`.counter`).css('color', 'rgb(182, 176, 167)')
-  }
+  // if (string.length > 140) {
+  //   $(`.counter`).css('color', 'red')
+  // }
+  // if (string.length <= 140) {
+  //   $(`.counter`).css('color', 'rgb(182, 176, 167)')
+  // }
 });
 
 
