e5d51620646e255d47fc75c67b61dfd9f417d950
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-28 22:56:10 +0000

    got counter to change to red when below 0 characters remaining

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -15,14 +15,15 @@ $(document).ready(function() {
   $('#tweet-text').keyup(() => {
     let string = $("#tweet-text").val();
     $('.counter').val(count - string.length)
+    if (string.length > 140) {
+      $(`.counter`).css('color', 'red')
+    }
+    if (string.length <= 140) {
+      $(`.counter`).css('color', 'rgb(182, 176, 167)')
+    }
   });
   //add in colour changing
-  // if (string.length > 140) {
-  //   $(`.counter`).css('color', 'red')
-  // }
-  // if (string.length <= 140) {
-  //   $(`.counter`).css('color', 'rgb(182, 176, 167)')
-  // }
+  
 });
 
 
